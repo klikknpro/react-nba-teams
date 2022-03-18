325431ab0f123ae855f936e5a3b7eb50bd38ddbd
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-03-18 08:48:54 +0100

    add vote button

--- reactinitial/src/components/Team.jsx
+++ reactinitial/src/components/Team.jsx
@@ -1,18 +1,32 @@
 import {React, useState} from 'react';
-import {Button} from "@mui/material";
+import http from "axios";
+import {Button, IconButton} from "@mui/material";
+import StarRateIcon from '@mui/icons-material/StarRate';
 
 const Team = ({team}) => {
   const [isShown, setIsShown] = useState(false);
+
+  const vote = async(playerId) => {
+    const response = await http.post("https://demoapi.com/api/vote",
+      {
+        id: playerId
+    });
+    console.log("did vote ", response);
+  };
 
   return (
     <div>
       <h2>{team.name}</h2>
       <h3>{team.stadium}</h3>
-      <Button onClick={() => setIsShown(!isShown)} >Show players</Button>
+      <Button onClick={() => setIsShown(!isShown)} size="small" color="warning" variant="outlined">
+        {!isShown ? "Show players" : "Hide players"}</Button>
       {isShown &&
         <ul>
           {team.franchisePlayers.map((player, i) => (
-            <li key={i}>{player.name}</li>
+            <li key={i} playerId={player.id}>
+              {player.name}
+              <IconButton onClick={() => vote(player.id)} color="warning" size="small"><StarRateIcon/></IconButton>
+            </li>
           ))}
         </ul>
       }
